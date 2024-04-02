6306d559b1ad1885b900278e37a7aa96d2886a2c
My <pingpengpongpog@gmail.com>
2024-04-02 00:17:56 -0700

    added print

--- load_tweets.py
+++ load_tweets.py
@@ -147,6 +147,7 @@ def insert_tweet(connection,tweet):
                 :withheld_in_countries
               ) on conflict do nothing;
             ''')
+            print(f"Attempting to insert user data for Tweet ID: {tweet.get('id', 'N/A')}, User ID: {tweet['user'].get('id', 'N/A')}")
 
         res = connection.execute(sql, {
             'id_users': tweet['user']['id'],
